65d5030d0764ffb569b75248d7209e115d3dff14
Alberto Rosales <arosales7@ucol.mx>
2021-10-07 03:07:45 -0500

    JSX y CreateElement

--- React-JS/my-app/src/App.js
+++ React-JS/my-app/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, Alberto!
-        </p>
+        <p>Hello, Alberto!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
