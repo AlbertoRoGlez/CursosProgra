09a4e2fdbc5cbadf543f8d8ca8a86bb9836288fe
Alberto Rosales <arosales7@ucol.mx>
2021-10-14 01:57:27 -0500

    Comienzo de Navbar

--- React-JS/badges/src/index.js
+++ React-JS/badges/src/index.js
@@ -1,20 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Badge from "./components/badge";
+import BadgeNew from "./pages/badgenew";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
+import Badge from "./components/badge";
 
 const container = document.getElementById("app");
 
 // Convertimos el Badge en un elemento y lo introducimos en el render.
-ReactDOM.render(
-  <Badge
-    firstName="Alberto"
-    lastName="Rosales"
-    jobTitle="Software Engineer"
-    twitter="AlbertoRoGlez"
-    avatarUrl="https://static.platzi.com/media/avatars/avatars/AlbertoRosalesGlez_2808dc89-13c3-4067-b9a1-a7f42c8a6968.jpg"
-  />,
-  container
-);
+// ReactDOM.render(
+//   <Badge
+//     firstName="Alberto"
+//     lastName="Rosales"
+//     jobTitle="Software Engineer"
+//     twitter="AlbertoRoGlez"
+//     avatarUrl="https://static.platzi.com/media/avatars/avatars/AlbertoRosalesGlez_2808dc89-13c3-4067-b9a1-a7f42c8a6968.jpg"
+//   />,
+//   container
+// );
 //En Badge del Dom.render colocamos la información de los props que estarán en el badge.js//
+//-------------------------------------------------------------------------------------------------------------------------------//
+ReactDOM.render(<BadgeNew />, container);
---
+++ React-JS/badges/src/pages/badgenew.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Navbar from "../components/navbar.js";
+
+class BadgeNew extends React.Component {
+  render() {
+    return (
+      //Navbar-----------------------------------------------------------------//
+      <div>
+        <Navbar />
+      </div>
+      //Navbar-----------------------------------------------------------------//
+    );
+  }
+}
+
+export default BadgeNew;
