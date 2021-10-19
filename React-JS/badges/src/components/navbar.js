14c7ded92abef0e98717605643182673e75d4090
Alberto Rosales <arosales7@ucol.mx>
2021-10-19 02:36:40 -0500

    NavbarTerminado

--- React-JS/badges/src/components/navbar.js
+++ React-JS/badges/src/components/navbar.js
@@ -1,8 +1,20 @@
 import React from "react";
+import Logo from "../images/logo.svg";
+import "./styles/navbar.css";
 
 class Navbar extends React.Component {
   render() {
-    return <p>Navbar</p>;
+    return (
+      <div className="Navbar">
+        <div className="container-fluid">
+          <a className="Navbar__brand" href="/">
+            <img className="Navbar__brand-logo" src={Logo} alt="Logo" />
+            <span className="fw-light">Platzi</span>
+            <span className="fw-bold">Conf</span>
+          </a>
+        </div>
+      </div>
+    );
   }
 }
 
--- React-JS/badges/src/pages/badgenew.js
+++ React-JS/badges/src/pages/badgenew.js
@@ -1,6 +1,8 @@
 import React from "react";
-
-import Navbar from "../components/navbar.js";
+import "../components/styles/badgenew.css";
+import header from "../images/badge-header.svg";
+import Navbar from "../components/navbar";
+import Badge from "../components/badge";
 
 class BadgeNew extends React.Component {
   render() {
@@ -8,6 +10,22 @@ class BadgeNew extends React.Component {
       //Navbar-----------------------------------------------------------------//
       <div>
         <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo" />
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge
+                firstName="Alberto"
+                lastName="Rosales"
+                twitter="AlbertoRoGlez"
+                jobTitle="Software Engineer"
+                avatarUrl="https://static.platzi.com/media/avatars/avatars/AlbertoRosalesGlez_2808dc89-13c3-4067-b9a1-a7f42c8a6968.jpg"
+              ></Badge>
+            </div>
+          </div>
+        </div>
       </div>
       //Navbar-----------------------------------------------------------------//
     );
