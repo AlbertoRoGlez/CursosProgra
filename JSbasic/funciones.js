4554f086e4565e687c9f6d5250a1c4085c2a48d7
Alberto Rosales <arosales7@ucol.mx>
2021-10-26 02:00:20 -0500

    Funciones

---
+++ JSbasic/funciones.js
@@ -0,0 +1,14 @@
+// Declarativas
+
+function miFuncionA() {
+  return 3;
+}
+
+miFuncionA();
+
+// Expresión
+var miFuncion = function (a = 1, b = 2) {
+  return a + b;
+};
+
+miFuncion();
---
+++ JSbasic/variables.js
@@ -0,0 +1,21 @@
+// Declarar
+var edad;
+
+//Inicializar
+edad = 30;
+
+// declarar e inicializar
+var edad = 30;
+
+// generar Arrays
+var elementos = ["Computadora", "Celular"];
+
+// objeto
+var persona = {
+  nombre: "Alberto",
+  edad: "30",
+};
+
+persona; // Alberto, 30
+persona[0]; // Alberto
+persona[1]; // 30
