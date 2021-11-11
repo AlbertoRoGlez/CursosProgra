391e2795be6b1582cc30d206621b9fdd968f80b2
Alberto Rosales <arosales7@ucol.mx>
2021-11-11 00:01:25 -0600

    La metaprogramación de Ruby es lo mejor

---
+++ Ruby/ciclos.rb
@@ -0,0 +1,20 @@
+# Ciclos
+
+# x = 1
+# while x < 5 do
+#     puts "Hola #{x}"
+#     x += 1
+# end
+
+# x = 1
+# loop do
+#     puts "Hola #{x}"
+#     x += 1
+#     break if x >= 5
+# end
+
+# for i in 1..4 do
+#     puts "Hola #{i}"
+# end
+
+# 4.times { |x| puts "Hola #{x}" }
---
+++ Ruby/poo.rb
@@ -0,0 +1,49 @@
+#-------------------------------------------------------------------#
+# class Persona 
+#     def self.suggested_names #Método que nos retornará nombres
+#         ["Juan", "Pedro", "Alberto"]
+#     end
+#     def initialize(name, age) #Constructor
+#       @name = name #Variable de instancia se inicializa con el parametro en el constructor
+#       @age = age
+#     end
+  
+#     def name #Getter
+#       @name
+#     end
+#     def age #Getter
+#         @age
+#       end
+  
+#     def name=(name) #Setter
+#        @name = name
+#        self
+#     end
+#     def age=(age) #Setter
+#         @age = age
+#         self
+#      end
+#   end
+#-------------------------------------------------------------------#
+# class Persona 
+#     attr_accessor(:name, :age) #Genera getters y setters
+#     def self.suggested_names #Método que nos retornará nombres
+#         ["Juan", "Pedro", "Alberto"]
+#     end
+#     def initialize(name, age) #Constructor
+#       @name = name #Variable de instancia se inicializa con el parametro en el constructor
+#       @age = age
+#     end
+#   end
+#-------------------------------------------------------------------#
+#-------------------------------------------------------------------#
+class Persona < Struct.new(:name, :age) #Struct declara el constructor, getters y setters
+    def self.suggested_names #Método que nos retornará nombres
+        ["Juan", "Pedro", "Alberto"]
+    end
+end
+#-------------------------------------------------------------------#
+
+  persona = Persona.new("pepe", 14)
+  puts persona.name
+  puts persona.age
