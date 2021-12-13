85b05ca6ac4c64d20d5cb8ce25508ad37931a8e8
Alberto Rosales <arosales7@ucol.mx>
2021-12-12 21:17:56 -0600

    RoR

---
+++ ReactNative/myApp/src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Estamos en HomeScreen</Text>
+      <Text>Estamos en HomeScreen</Text>
+      <Text>Estamos en HomeScreen</Text>
+      <Text>Estamos en HomeScreen</Text>
+      <Text>Estamos en HomeScreen</Text>
+      <Text>Estamos en HomeScreen</Text>
+      <Text>Estamos en HomeScreen</Text>
+      <Text>Estamos en HomeScreen</Text>
+      <Text>Estamos en HomeScreen</Text>
+      <Text>Estamos en HomeScreen</Text>
+      <Text>Estamos en HomeScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
---
+++ ReactNative/myApp/src/screens/SettingsScreen.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export const SettingsScreen = () => {
+  return (
+    <View>
+      <Text>Estamos en SettingsScreen</Text>
+      <Text>Estamos en SettingsScreen</Text>
+      <Text>Estamos en SettingsScreen</Text>
+      <Text>Estamos en SettingsScreen</Text>
+      <Text>Estamos en SettingsScreen</Text>
+      <Text>Estamos en SettingsScreen</Text>
+      <Text>Estamos en SettingsScreen</Text>
+      <Text>Estamos en SettingsScreen</Text>
+      <Text>Estamos en SettingsScreen</Text>
+      <Text>Estamos en SettingsScreen</Text>
+      <Text>Estamos en SettingsScreen</Text>
+      <Text>Estamos en SettingsScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
