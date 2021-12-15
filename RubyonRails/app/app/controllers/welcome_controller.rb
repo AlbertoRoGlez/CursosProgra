140135dc9bebbcd637946dcd3854ed5fcc824ce6
Alberto Rosales <arosales7@ucol.mx>
2021-12-14 18:54:04 -0600

    Modelo, Vista, Control

--- RubyonRails/app/app/controllers/welcome_controller.rb
+++ RubyonRails/app/app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
     def hello
+        name = Pet.first.name
+        breed = Pet.first.breed
+        @variable = "Hola soy #{name}, y soy un #{breed}"
     end
 end
