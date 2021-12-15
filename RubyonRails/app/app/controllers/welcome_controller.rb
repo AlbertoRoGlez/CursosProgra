353ae142c227588ffbfd45cb3b0b1cdef5c79c2b
Alberto Rosales <arosales7@ucol.mx>
2021-12-15 00:49:29 -0600

    Credentials

--- RubyonRails/app/app/controllers/welcome_controller.rb
+++ RubyonRails/app/app/controllers/welcome_controller.rb
@@ -3,5 +3,6 @@ class WelcomeController < ApplicationController
         name = Pet.first.name
         breed = Pet.first.breed
         @variable = "Hola soy #{name}, y soy un #{breed}"
+        @credentials = Rails.application.credentials.hello
     end
 end
