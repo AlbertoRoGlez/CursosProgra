9d797c949660b9bb0fd9cb3e98af0e29eb6f0565
Alberto Rosales <arosales7@ucol.mx>
2021-12-16 22:30:17 -0600

    SimplForm

--- RubyonRails/app/db/schema.rb
+++ RubyonRails/app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_000203) do
+ActiveRecord::Schema.define(version: 2021_12_17_032932) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2021_12_16_000203) do
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "breed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.text "about_me"
+    t.integer "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
